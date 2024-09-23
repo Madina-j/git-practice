46e43b952cd0573cd307769e32d681fa099182ef
luqi <lq.xie.work@gmail.com>
2024-09-23 15:06:50 -0700

    completes Luqi's code for function d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,16 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    # Luqi
+    max_num = 0
+    
+    for i in range(len(numbers)):
+        if numbers[i] > max_num:
+            max_num = numbers[i]
+
+    return max_num
+
+
 
 
 if __name__ == "__main__":
